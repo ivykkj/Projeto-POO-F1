3e1b932ab7e520295a6671e784ef9e366aaf7059
Cauan Melo <cauandemelo2018@gmail.com>
2025-05-21 21:13:32 -0300

    muita coisa

--- classes.py
+++ classes.py
@@ -3,13 +3,13 @@ from abc import ABC, abstractmethod
 class Funcionario(ABC):
     contador_funcionarios = 0
 
-    def __init__(self, nome, email, cpf, data_contratacao, salario, id_equipe):
+    def __init__(self, nome, email, cpf, data_contratacao, salario, equipe):
         self.nome = nome
         self.email = email
         self.cpf = cpf
         self.data_contratacao = data_contratacao
         self.salario = salario
-        self.id_equipe = id_equipe
+        self.equipe = equipe
         Funcionario.contador_funcionarios += 1
 
     @property
@@ -36,17 +36,20 @@ class Funcionario(ABC):
         pass
     
 class Piloto(Funcionario):
-    def __init__(self, nome, email, cpf, data_contratacao, salario, id_equipe, numero_carro, nacionalidade):
-        super().__init__(nome, email, cpf, data_contratacao, salario, id_equipe)
+    def __init__(self, nome, email, cpf, data_contratacao, salario, equipe, numero_carro, nacionalidade):
+        super().__init__(nome, email, cpf, data_contratacao, salario, equipe)
         self.numero_carro = numero_carro
         self.nacionalidade = nacionalidade
+        
+    def contar_poles(self, qualificacao):
+        return sum(1 for q in qualificacao if q.obter_pole_position() == self)
 
     def info(self):
-        print(f'Piloto: {self.nome}, Carro #{self.numero_carro}, Nacionalidade: {self.nacionalidade}, Equipe: {self.id_equipe} ')
+        print(f'Piloto: {self.nome}, Carro #{self.numero_carro}, Nacionalidade: {self.nacionalidade}, Equipe: {self.equipe} ')
 
 class Engenheiro(Funcionario):
-    def __init__(self, nome, email, cpf, data_contratacao, salario, id_equipe, especialidade, nivel_experiencia):
-        super().__init__(nome, email, cpf, data_contratacao, salario, id_equipe)
+    def __init__(self, nome, email, cpf, data_contratacao, salario, equipe, especialidade, nivel_experiencia):
+        super().__init__(nome, email, cpf, data_contratacao, salario, equipe)
         self.especialidade = especialidade
         self.nivel_experiencia = nivel_experiencia
 
@@ -55,73 +58,133 @@ class Engenheiro(Funcionario):
             self.nivel_experiencia += 1
             print(f"{self.nome} promovido para nível {self.nivel_experiencia}")
 
-
     def info(self):
-        print(f'Engenheiro: {self.nome}, Especialidade: {self.especialidade}, Nível: {self.nivel_experiencia}, Equipe: {self.id_equipe}')
+        print(f'Engenheiro: {self.nome}, Especialidade: {self.especialidade}, Nível: {self.nivel_experiencia}, Equipe: {self.equipe}')
 
 class ChefeEquipe(Funcionario):
-    def __init__(self, nome, email, cpf, data_contratacao, salario, id_equipe, anos_experiencia):
-        super().__init__(nome, email, cpf, data_contratacao, salario, id_equipe)
+    def __init__(self, nome, email, cpf, data_contratacao, salario, equipe, anos_experiencia):
+        super().__init__(nome, email, cpf, data_contratacao, salario, equipe)
         self.anos_experiencia = anos_experiencia
 
     def info(self):
-        print(f'Chefe de Equipe: {self.nome}, Experiência: {self.anos_experiencia} anos, Equipe: {self.id_equipe}')
+        print(f'Chefe de Equipe: {self.nome}, Experiência: {self.anos_experiencia} anos, Equipe: {self.equipe}')
 
 class Equipe:
-    def __init__(self, nome, pais, chefe_id):
+    def __init__(self, nome, pais, chefe):
         self.nome = nome
         self.pais = pais
-        self.chefe_id = chefe_id
+        self.chefe = chefe
+        self.pilotos = []
+        self.engenheiros = []
+
+    def adicionar_piloto(self, piloto):
+        self.pilotos.append(piloto)
 
 class Carro:
-    def __init__(self, modelo, motor, ano, equipe_id):
+    def __init__(self, modelo, motor, ano, equipe):
         self.modelo = modelo
         self.motor = motor
         self.ano = ano
-        self.equipe_id = equipe_id
+        self.equipe = equipe
 
 class TreinoLivre:
-    def __init__(self, data, melhor_volta, piloto_id, carro_id):
+    def __init__(self, data, melhor_volta, sessao , piloto, carro, corrida):
         self.data = data
         self.melhor_volta = melhor_volta
-        self.piloto_id = piloto_id
-        self.carro_id = carro_id
+        self.piloto = piloto
+        self.carro = carro
+        self.sessao = sessao  
+        self.corrida = corrida
+
+    @classmethod
+    def melhor_treino(cls, treinos):
+        if not treinos:
+            return None
+
+        melhor = treinos[0]
+        for treino in treinos[1:]:
+            if treino.melhor_volta < melhor.melhor_volta:
+                melhor = treino
+        return melhor
 
 class Temporada:
     def __init__(self, ano):
         self.ano = ano
+    
+    def adicionar_corrida(self, corrida):
+        self.corridas.append(corrida)
+
+    def adicionar_resultado(self, resultado):
+        self.resultados.append(resultado)
+
+    def definir_campeao(self):
+        pontos_por_piloto = {}
+
+        for resultado in self.resultados:
+            piloto = resultado.piloto
+            pontos_por_piloto[piloto] = pontos_por_piloto.get(piloto, 0) + resultado.pontos
+
+        campeao = None
+        maior_pontuacao = -1
+
+        for piloto in pontos_por_piloto:
+            if pontos_por_piloto[piloto] > maior_pontuacao:
+                maior_pontuacao = pontos_por_piloto[piloto]
+                campeao = piloto
+
+        return campeao, maior_pontuacao
 
 class Qualificacao:
-    def __init__(self, corrida_id, piloto_id, tempo_q1, tempo_q2, tempo_q3):
-        self.corrida_id = corrida_id
-        self.piloto_id = piloto_id
-        self.tempo_q1 = tempo_q1
-        self.tempo_q2 = tempo_q2
-        self.tempo_q3 = tempo_q3
+    def __init__(self, corrida, piloto, q1, q2, q3):
+        self.corrida = corrida
+        self.piloto = piloto
+        self.q1 = q1
+        self.q2 = q2
+        self.q3 = q3
 
-    
-    
+    def adicionar_tempo(self, fase, piloto, tempo):
+        if fase == 'Q1':
+            self.q1.append((piloto, tempo))
+        elif fase == 'Q2':
+            self.q2.append((piloto, tempo))
+        elif fase == 'Q3':
+            self.q3.append((piloto, tempo))
 
+    def determinar_grid_largada(self):
+        grid = []
+
+        for i in range(len(self.q3)):
+            menor = self.q3[0]
+            for tempo in self.q3:
+                if tempo[1] < menor[1]:
+                    menor = tempo
+            grid.append(menor)
+            self.q3.remove(menor)
+        return grid
+    
+    def obter_pole_position(self):
+        grid = self.definir_grid_largada()
+        return grid[0][0] 
+    
 class Corrida:
-    def __init__(self, nome, data, pais, clima, temporada_id, pontos):
+    pontos = [25, 18, 15, 12, 10, 8, 6, 4, 2, 1]
+
+    def __init__(self, nome, data, pais, clima, temporada):
         self.nome = nome
         self.data = data
         self.pais = pais
-        self.temporada_id = temporada_id
+        self.temporada = temporada
         self.clima = clima
-        self.pontos = [25, 18, 15, 12, 10, 8, 6, 4, 2, 1]
-
-
+      
     def calcular_pontuacao(self, posicao):
         if 1 <= posicao <= 10:
-            return self.pontos[posicao - 1]
+            return Corrida.pontos[posicao - 1]
         return 0
 
 class ResultadoCorrida:
-    def __init__(self, piloto_id, corrida_id, equipe_id, posicao, pontos):
-        self.piloto_id = piloto_id
-        self.corrida_id = corrida_id
-        self.equipe_id = equipe_id
+    def __init__(self, piloto, corrida, equipe, posicao, pontos):
+        self.piloto = piloto
+        self.corrida = corrida
+        self.equipe = equipe
         self.posicao = posicao
-        self.pontos = pontos 
-
+        self.pontos = pontos
